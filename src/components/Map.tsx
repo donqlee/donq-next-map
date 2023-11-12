db5aef53325c510b054aed2cc8c1789d1acb7c97
donqlee <donqlee88@gmail.com>
2023-11-12 22:30:19 +0900

    feat: 서울공공데이터 import 및 카카오 기본 마커로 띄우기

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,7 @@
 /* global kakao*/
+
 import Script from "next/script";
+import * as stores from "@/data/store_data.json";
 
 declare global {
   interface Window {
@@ -7,17 +9,31 @@ declare global {
   }
 }
 
+const DEFAULT_LAT = 37.49762503;
+const DEFAULT_LNG = 127.03088379;
+
 export default function Map() {
   const loadKakaoMap = () => {
     //kakao map 로드
     window.kakao.maps.load(() => {
       const mapContainer = document.getElementById("map");
       const mapOption = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
         level: 3,
       };
+      const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-      new window.kakao.maps.Map(mapContainer, mapOption);
+      //식당 데이터 마커 띄우기
+      stores?.["DATA"]?.map((store) => {
+        const markerPosition = new window.kakao.maps.LatLng(
+          store?.y_dnts,
+          store?.x_cnts
+        );
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map);
+      });
     });
   };
   return (
